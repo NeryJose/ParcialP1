4ec6b42ed153c64eca8ec9d5cb2f471d04635b10
nery jose martinez bolaños <neryjm98@gmail.com>
2022-03-04 15:09:23 -0600

    Agregar archivos de proyecto.

---
+++ ParcialP1/ParcialP1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ParcialP1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private Entry GetCantidad()
+        {
+            return Cantidad;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble (Cantidad.Text);
+
+            double dol = n1 * 7.50;
+
+
+            DisplayAlert("Resultado", dol.ToString(), "total");
+        }
+    }
+}
+
+
+
